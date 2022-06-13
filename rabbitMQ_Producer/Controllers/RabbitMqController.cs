23bb5f999067799d080436d5894ec6d91e06769e
SMM3\smm3 <subbcoder@subbnet.ru>
2022-06-13 23:15:36 +0300

    Добавьте файлы проекта.

---
+++ rabbitMQ_Producer/Controllers/RabbitMqController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rabbitMQ.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RabbitMqController : ControllerBase
+	{
+		private readonly IRabbitMqService _mqService;
+
+		public RabbitMqController(IRabbitMqService mqService)
+		{
+			_mqService = mqService;
+		}
+
+		[Route("[action]/{message}")]
+		[HttpGet]
+		public IActionResult SendMessage(string message)
+		{
+			_mqService.SendMessage(message);
+
+			return Ok("Сообщение отправлено");
+		}
+	}
+}
---
+++ rabbitMQ_Producer/IRabbitMqService.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace rabbitMQ
+{
+    public interface IRabbitMqService
+	{
+		void SendMessage(object obj);
+		void SendMessage(string message);
+	}
+
+	public class RabbitMqService : IRabbitMqService
+	{
+		public void SendMessage(object obj)
+		{
+			var message = JsonSerializer.Serialize(obj);
+			SendMessage(message);
+		}
+
+		public void SendMessage(string message)
+		{
+			// Не забудьте вынести значения "localhost" и "MyQueue"
+			// в файл конфигурации
+			var factory = new ConnectionFactory() { HostName = "10.10.11.18" };
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare(queue: "MyQueue",
+							   durable: false,
+							   exclusive: false,
+							   autoDelete: false,
+							   arguments: null);
+
+				var body = Encoding.UTF8.GetBytes(message);
+
+				channel.BasicPublish(exchange: "",
+							   routingKey: "MyQueue",
+							   basicProperties: null,
+							   body: body);
+			}
+		}
+	}
+}
